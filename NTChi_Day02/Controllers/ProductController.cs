ac79c8cc2c1b5712d55df2e6b8af96243a7034d7
nguyenthuychi0306 <uynci072@gmail.com>
2026-08-23 01:50:30 +0700

    Thêm thư mục NTChi_Day02

---
+++ NTChi_Day02/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NTChi_Day02.Models; 
+namespace NTChi_Day02.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            var products = new List<Product>
+            {
+                new Product { Id = 1, Name = "Product 1", Price = 100000, CreatedAt = DateTime.Parse("2026-06-03"), Image = "anh1.webp" },
+                new Product { Id = 2, Name = "Product 2", Price = 120000, CreatedAt = DateTime.Parse("2025-12-24"), Image = "anh2.webp" },
+                new Product { Id = 3, Name = "Product 3", Price = 50000, CreatedAt = DateTime.Parse("2024-09-25"), Image = "anh3.jpg " },
+                new Product { Id = 4, Name = "Product 4", Price = 650000, CreatedAt = DateTime.Parse("2026-02-02"), Image = "anh4.jpg " }
+            };
+
+            return View(products);
+        }
+    }
+}
---
+++ NTChi_Day02/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace NTChi_Day02.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string Image { get; set; }
+    }
+}
